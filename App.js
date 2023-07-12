deb00b942c44d251dabd665247cfa1b20f015419
iamtanmayrudr <tanmay.rudra@codeclouds.in>
2023-07-13 03:09:08 +0530

    Native Practice Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './src/component/Header'
+
+const App = ()=>{
+  return(
+    <Header/>
+  )
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+function Header() {
+  return (
+    <View>
+        <Text>Test Data</Text>
+    </View>
+  )
+}
+
+export default Header
